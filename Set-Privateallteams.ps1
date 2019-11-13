9283b7bcc1d0d6e4a2b7bfce14b1f280ef8621f8
Shomi-Nijo <34257929+Shomi-Nijo@users.noreply.github.com>
2019-11-14 08:30:20 +0900

    Update Set-Privateallteams.ps1
    powershellのつよいところ

--- Set-Privateallteams.ps1
+++ Set-Privateallteams.ps1
@@ -1,10 +1,23 @@
 # 関数
 
+# Teams接続
+try
+{
+    #WriteLog $logStatus_Inf "Teamsへの接続を開始します。"
+    Import-Module MicrosoftTeams
+    Connect-MicrosoftTeams -Credential $credential
+}
+catch
+{
+    #WriteLog $logStatus_err ("Teamsに接続できませんでした。" + $error[0].Exception.Message)
+    break
+}
 
-# チームを取得
-Get-team
 
-# 公開範囲がPublicの場合、Privateに変更する
+# publicチームを取得
+$TeamList = Get-Team| Where-Object {$_.Visibility -eq "Public"}
+
+# 公開範囲をPrivateに変更する
 Set-Team -GroupId **** -Visibility Private
 
 # 終了処理
